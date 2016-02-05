cc2e99f3bf3adcfd0d3ac145ac3d3846ee7f6ef5
Matthias Dittrich <matthi.d@gmail.com>
2016-02-05 22:34:03 +0100

    initial commit.

---
+++ src/SharedAssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyCompanyAttribute("Yaaf.Xmpp.Data")>]
+[<assembly: AssemblyProductAttribute("Yaaf.Xmpp.Data")>]
+[<assembly: AssemblyCopyrightAttribute("Yaaf.Xmpp.Data Copyright © Matthias Dittrich 2015")>]
+[<assembly: AssemblyVersionAttribute("0.2.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.2.1"
